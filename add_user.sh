57c22b38eef0dd4ae6a83b984db19175737a0245
ymd65536 <ymd65536@gmail.com>
2022-05-23 00:53:47 +0900

    パスワードをハッシュ化

--- add_user.sh
+++ add_user.sh
@@ -5,6 +5,6 @@
 username="${1}"
 
 # some operation
-sudo useradd -p "password" -d /home/$username -G jupyter $username
+sudo useradd -p $(perl -e 'print crypt("password", "\$6\$salt03")') -d /home/$username -G jupyter $username
 sudo mkdir /home/$username/notebooks
 sudo chmod 777 /home/$username/notebooks
